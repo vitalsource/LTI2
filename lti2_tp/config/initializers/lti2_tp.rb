947d8c19051d2c6d733c4be059717db7c7a4fded
John Taylor <john.taylor@ingramcontent.com>
2017-02-09 12:04:58 -0500

    remove database check from initializer

--- lti2_tp/config/initializers/lti2_tp.rb
+++ lti2_tp/config/initializers/lti2_tp.rb
@@ -4,8 +4,6 @@ include WireLogSupport
 NONCE_TIME_TO_LIVE = 300  # seconds
 Rails.application.config.nonce_cache = Cache.new :ttl => NONCE_TIME_TO_LIVE
 
-if ActiveRecord::Base.connection.data_source_exists? 'lti2_tp_registries'
-  Rails.application.config.tool_provider_registry = Lti2Tp::ToolProviderRegistry.new
-end
+Rails.application.config.tool_provider_registry = Lti2Tp::ToolProviderRegistry.new
 
 Rails.application.config.wire_log = WireLog.new "ToolProvider", File.expand_path("../tc_sample_app/public/wirelog.html")
